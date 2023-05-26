f4185a9f09684b12b964ba56170ffd114d46d0e3
Julzpeter <chepngetichjuliet@gmail.com>
2023-05-26 13:05:30 +0300

    create the models.py file

--- kapsiya/models.py
+++ kapsiya/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from phone_field import PhoneField
 
 # Create your models here.
+class Admin(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    password = models.PasswordField(max_length=50)
+    phonenumber = models.PhoneField(max_length=10, null=False,blank=False,unique=True)
+    
