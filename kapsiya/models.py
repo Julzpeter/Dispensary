d479e5a1a011b159890b37efd9f75e54b78b205e
Julzpeter <chepngetichjuliet@gmail.com>
2023-05-26 13:23:10 +0300

    install the phone number module, register it to the settings and change the import

--- kapsiya/models.py
+++ kapsiya/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-from phone_field import PhoneField
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 class Admin(models.Model):
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
     username = models.CharField(max_length=50)
-    password = models.PasswordField(max_length=50)
-    phonenumber = models.PhoneField(max_length=10, null=False,blank=False,unique=True)
+    password = models.CharField(max_length=50)
+    phone_number = PhoneNumberField(max_length=10, null=False,blank=False,unique=True)
+
     
