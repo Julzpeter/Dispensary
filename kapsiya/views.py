0ea2bbe8f7b4de3d656db29d6f75cb0a32f376b0
Julzpeter <chepngetichjuliet@gmail.com>
2023-05-26 11:41:59 +0300

    create a home view function

--- kapsiya/views.py
+++ kapsiya/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import Response
+from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return
+    return HttpResponse('<h1>Welcome to Kapsiya Dispensary Management Syste</h1>')
