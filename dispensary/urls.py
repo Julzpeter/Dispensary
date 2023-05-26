2561aa4c837952fd7a184884a2c849ce9f16fbe4
Julzpeter <chepngetichjuliet@gmail.com>
2023-05-26 12:48:02 +0300

    change the url patterns for the app

--- dispensary/urls.py
+++ dispensary/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('kapsiya/',include('kapsiya.urls')),
+    path('',include('kapsiya.urls')),
 
 ]
