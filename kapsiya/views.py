f4abaa72cf1f44798c6a06fb2bccf24ae29c8629
Julzpeter <chepngetichjuliet@gmail.com>
2023-06-04 13:30:41 +0300

    make a correction on the adminclick view function

--- kapsiya/views.py
+++ kapsiya/views.py
@@ -12,7 +12,7 @@ def about(request):
 
 #views for showing signup/login button for admin (by submit)
 def adminclick_view(request):
-    if request.user.is_authenticated():
+    if request.user.is_authenticated:
         return HttpResponseRedirect('afterlogin')
     return render(request, 'adminclick.html')
 
