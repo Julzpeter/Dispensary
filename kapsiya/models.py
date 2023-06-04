9bb6b2a1fc35da346d42e1c4620a7c83840b5528
Julzpeter <chepngetichjuliet@gmail.com>
2023-06-04 13:21:57 +0300

    create table for the doctor

--- kapsiya/models.py
+++ kapsiya/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Admin(models.Model):
@@ -9,6 +11,20 @@ class Admin(models.Model):
     password = models.CharField(max_length=50)
     phone_number = PhoneNumberField(max_length=10, null=False,blank=False,unique=True)
 
-    
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='profile_pic/DoctorProfilePic/',null=True,blank=True)
+    address = models.CharField(max_length=40)
+    phone_number = PhoneNumberField(max_length=10, null=False,blank=False,unique=True)
+
+    @property
+    def get_name(self):
+        return self.user.first_name+ "" +self.user.last_name 
+    @property
+    def get_id(self):
+        return self.user.id
+
+    def __str__(self):
+        return "{} ({})".format(self.user.first_name) 
 
     
