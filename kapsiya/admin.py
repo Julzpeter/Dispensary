6ff5127de5b3dedf2e6f96013460e1fb357d066a
Julzpeter <chepngetichjuliet@gmail.com>
2023-06-04 14:26:44 +0300

    register models to the admin.py file

--- kapsiya/admin.py
+++ kapsiya/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Admin, Doctor
 
 # Register your models here.
+class Admin(admin.ModelAdmin):
+    pass
+admin.site.register(Admin, Admin)
+
+class Doctor(admin.ModelAdmin):
+    pass
+admin.site.register(Doctor,Doctor)
