393d1fa97c9e6017be410783315787a91fcf845f
Julzpeter <chepngetichjuliet@gmail.com>
2023-06-11 16:47:47 +0300

    add url path for the doctorclick

--- kapsiya/urls.py
+++ kapsiya/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='kapsiya-home'),
-    path('adminclick/', views.adminclick_view, name='kapsiya-admin'),
+    path('adminclick/', views.adminclick_view),
+    path('doctorclick/', views.doctorclick_view),
+
+
     path('adminsignup/', views.admin_signup_view, name='adminsignup'),
     path('adminlogin/', views.admin_login, name='adminlogin'),
     
